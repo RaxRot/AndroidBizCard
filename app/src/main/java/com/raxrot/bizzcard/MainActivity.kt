04c355784c298cc1722c4ffe7a1e6e8f758a49f7
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-11-28 18:44:24 +0000

    Finish App

--- app/src/main/java/com/raxrot/bizzcard/MainActivity.kt
+++ app/src/main/java/com/raxrot/bizzcard/MainActivity.kt
@@ -7,15 +7,17 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -28,9 +30,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.raxrot.bizzcard.ui.theme.BizzCardTheme
@@ -59,6 +64,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClickedState = remember { mutableStateOf(false) }
+
    Surface(modifier = Modifier
        .fillMaxWidth()
        .fillMaxHeight()) {
@@ -73,7 +80,8 @@ fun CreateBizCard() {
            Column(modifier = Modifier
                .height(400.dp)
                .fillMaxWidth()
-               .padding(top = 30.dp).background(Color.Cyan),
+               .padding(top = 30.dp)
+               .background(Color.Cyan),
                verticalArrangement = Arrangement.Top,
                horizontalAlignment = Alignment.CenterHorizontally) {
 
@@ -84,23 +92,33 @@ fun CreateBizCard() {
 
                CreateInfo()
 
-               Button(
-                   onClick = { /*TODO*/ },
-                   modifier = Modifier.padding(top = 15.dp),
-                   shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-                   colors = ButtonDefaults.buttonColors(containerColor = Color.Blue) // Цвет фона кнопки
-               ) {
-                   Text(
-                       text = "Show",
-                       fontSize = 25.sp,
-                       modifier = Modifier.padding(3.dp)
-                   )
-               }
+               ShowButton(buttonClickedState)
+           }
+
+           if (buttonClickedState.value) {
+               Content()
            }
 
        }
 
    }
+}
+
+@Composable
+private fun ShowButton(buttonClickedState: MutableState<Boolean>) {
+
+    Button(
+        onClick = { buttonClickedState.value=!buttonClickedState.value},
+        modifier = Modifier.padding(top = 15.dp),
+        shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+    ) {
+        Text(
+            text = if (buttonClickedState.value) "Hide" else "Show",
+            fontSize = 25.sp,
+            modifier = Modifier.padding(3.dp)
+        )
+    }
 }
 
 @Composable
@@ -124,6 +142,63 @@ private fun CreateInfo() {
             fontSize = 18.sp,
             modifier = Modifier.padding(top = 5.dp)
         )
+    }
+}
+
+
+
+@Composable
+fun Content() {
+    val projects = listOf("Project1", "Project2", "Project3", "Project4", "Project5", "Project6")
+    val images = listOf(
+        R.drawable.cat,
+        R.drawable.av,
+        R.drawable.cat,
+        R.drawable.av,
+        R.drawable.cat
+    )
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .padding(5.dp)){
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxHeight()
+            .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(5.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.Green)
+        ) {
+            Portfolio(data = projects,images=images)
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>, images: List<Int>) {
+    val projectWithImages = data.zip(images) //create pair data with images
+
+    LazyColumn {
+        items(projectWithImages) { (project, image) ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .background(Color.LightGray)
+                    .height(100.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = image),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(85.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+                Text(text = project, fontSize = 20.sp)
+            }
+        }
     }
 }
 
