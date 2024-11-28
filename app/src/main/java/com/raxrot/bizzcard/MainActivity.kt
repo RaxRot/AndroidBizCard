5ad4de9d72c51dcf1be0249b88d21ae3d3921493
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-11-28 15:06:05 +0000

    Add simple card

--- app/src/main/java/com/raxrot/bizzcard/MainActivity.kt
+++ app/src/main/java/com/raxrot/bizzcard/MainActivity.kt
@@ -3,14 +3,25 @@ package com.raxrot.bizzcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.raxrot.bizzcard.ui.theme.BizzCardTheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   CreateBizCard()
                 }
             }
         }
@@ -30,18 +41,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateBizCard() {
+   Surface(modifier = Modifier
+       .fillMaxWidth()
+       .fillMaxHeight()) {
 
+       Card(modifier = Modifier.width(200.dp)
+           .height(390.dp)
+           .padding(15.dp),
+            elevation = CardDefaults.cardElevation(5.dp),
+           shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+           colors = CardDefaults.cardColors(containerColor = Color.White)
+       ){
+
+       }
+
+   }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    BizzCardTheme {
-        Greeting("Android")
-    }
+fun CreateBizCardPreview() {
+    CreateBizCard()
 }
+
+
+
