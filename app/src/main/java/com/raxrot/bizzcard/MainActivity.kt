53382e7cec339802e514965079f61e2d39e054fd
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-11-28 15:37:04 +0000

    Add image profile

--- app/src/main/java/com/raxrot/bizzcard/MainActivity.kt
+++ app/src/main/java/com/raxrot/bizzcard/MainActivity.kt
@@ -3,25 +3,36 @@ package com.raxrot.bizzcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.raxrot.bizzcard.ui.theme.BizzCardTheme
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,7 +57,8 @@ fun CreateBizCard() {
        .fillMaxWidth()
        .fillMaxHeight()) {
 
-       Card(modifier = Modifier.width(200.dp)
+       Card(modifier = Modifier
+           .width(200.dp)
            .height(390.dp)
            .padding(15.dp),
             elevation = CardDefaults.cardElevation(5.dp),
@@ -54,9 +66,39 @@ fun CreateBizCard() {
            colors = CardDefaults.cardColors(containerColor = Color.White)
        ){
 
+           Column(modifier = Modifier
+               .height(300.dp)
+               .fillMaxWidth()
+               .padding(top = 30.dp),
+               verticalArrangement = Arrangement.Top,
+               horizontalAlignment = Alignment.CenterHorizontally) {
+
+               CreateImageProfile()
+               Divider(color = Color.Black,
+                   thickness =1.dp)
+           }
+
        }
 
    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier=Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 4.dp
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.av),
+            contentDescription = null, modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Preview
